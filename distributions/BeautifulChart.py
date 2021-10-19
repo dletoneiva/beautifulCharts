f897d8f8ef4f175ad06fc0cff5e5a714709c6748
Daniel Leto <daniel.leto@dtidigital.com.br>
2021-10-19 17:21:55 -0300

    .

---
+++ distributions/BeautifulChart.py
@@ -0,0 +1,40 @@
+class Chart: 
+    def __init__(self):
+        """
+        Initializes the chart object with default values.
+
+        Attributes:
+
+            primaryColorPalette (list of 5 hex): list containing the primary 
+            color palette (RGB)
+
+            color_palette (list of 5 hex): list containing the secondary color 
+            palette (RGB)
+        """
+        pass
+
+    def changeColorPalettePrimary(self, color_palette):
+        """
+        Changes the primary color palette
+
+        Args:
+            color_palette: list of 5 hex codes for RGB
+        
+        Returns:
+            None
+
+        """
+        pass
+
+    def changeColorPaletteSecondary(self, color_palette):
+        """
+        Changes the secondary color palette
+
+        Args:
+            color_palette: list of 5 hex codes for RGB
+        
+        Returns:
+            None
+
+        """
+        pass
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='beautifulCharts',
+      version='0.1',
+      description='Charts used in everyday activities',
+      packages=['distributions'],
+      zip_safe=False)
