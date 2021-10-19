6bc0cd46d6a14e253c6b9dc4e19a7b55f62fa0b3
Daniel Leto <daniel.leto@dtidigital.com.br>
2021-10-19 18:09:09 -0300

    added first version of stackedBar

--- distributions/BeautifulChart.py
+++ distributions/BeautifulChart.py
@@ -8,14 +8,23 @@ class Chart:
             primaryColorPalette (list of 5 hex): list containing the primary 
             color palette (RGB)
 
-            color_palette (list of 5 hex): list containing the secondary color 
-            palette (RGB)
+            secondaryColorPalette (list of 4 hex): list containing the secondary 
+            color palette (RGB)
         """
-        pass
+        self.primaryColorPalette = ['#DB6B2E',
+                                    '#E28958',
+                                    '#E9A682',
+                                    '#F1C4AB',
+                                    '#F8E1D5'] # set in orange colors
+        
+        self.secondaryColorPalette = ['#CF3779',
+                                      '#ADC242',
+                                      '#432466',
+                                      '#55A8B5'] # set in happy colors
 
     def changeColorPalettePrimary(self, color_palette):
         """
-        Changes the primary color palette
+        Changes the primary color palette, low conspicuity uses
 
         Args:
             color_palette: list of 5 hex codes for RGB
@@ -24,14 +33,19 @@ class Chart:
             None
 
         """
-        pass
+        if len(color_palette)!= 5:
+            return 'Incompatible list size.'
+        else:
+            self.primaryColorPalette = color_palette
+            return self.primaryColorPalette
+        
 
     def changeColorPaletteSecondary(self, color_palette):
         """
-        Changes the secondary color palette
+        Changes the secondary color palette, high conspicuity uses
 
         Args:
-            color_palette: list of 5 hex codes for RGB
+            color_palette: list of 4 hex codes for RGB
         
         Returns:
             None
